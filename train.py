605fa8fa49628ed722d0553ee9acf01ce6ee27fb
MlLearnerAkash <manna.phys@gmail.com>
2024-07-25 05:53:44 +0000

    modified: train file adding more hyperparameters

--- train.py
+++ train.py
@@ -4,18 +4,19 @@ from ultralytics import YOLO
 def main(
     base_model: str,
     datasets: str = "./datasets/data.yaml",
-    epochs: int = 400,
+    epochs: int = 500,
     imgsz: int = 1024,
     batch: int = 16,
     dropout: float = 0.0,
-    resume: bool = False,
+    resume: bool = True,
     device = "0",
+    name: str= "background-finetune",
 ):
     try:
         from clearml import Task
 
         Task.init(
-            project_name="yolo-doclaynet",
+            project_name="yolo-doclaynet-background-230724",
             task_name=f"base-model-{base_model}-epochs-{epochs}-imgsz-{imgsz}-batch-{batch}",
         )
     except ImportError:
@@ -30,6 +31,7 @@ def main(
         dropout=dropout,
         resume=resume,
         device = device,
+        name= name,
 
     )
     print(results)
