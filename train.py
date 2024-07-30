8f3bfb5f0808eed5cc5473f7475fa7043a02ea43
MlLearnerAkash <manna.phys@gmail.com>
2024-07-30 05:28:01 +0000

    Modified for publaynet

--- train.py
+++ train.py
@@ -3,20 +3,20 @@ from ultralytics import YOLO
 
 def main(
     base_model: str,
-    datasets: str = "./datasets/data.yaml",
-    epochs: int = 500,
+    datasets: str = "/home/akash/ws/dataset/datasets/publaynet/JSON2YOLO/new_dir/dataset.yaml",
+    epochs: int = 150,
     imgsz: int = 1024,
     batch: int = 16,
     dropout: float = 0.0,
-    resume: bool = True,
+    resume: bool = False,
     device = "0",
-    name: str= "background-finetune",
+    name: str= "publaynet-finetune",
 ):
     try:
         from clearml import Task
 
         Task.init(
-            project_name="yolo-doclaynet-background-230724",
+            project_name="yolo-publaynet-30072024",
             task_name=f"base-model-{base_model}-epochs-{epochs}-imgsz-{imgsz}-batch-{batch}",
         )
     except ImportError:
